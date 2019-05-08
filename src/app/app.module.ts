4314f690efb7035a357610962f71bcf1eb99400c
Charline Fazentieux <chafazentieuxpro@gmail.com>
2019-05-09 00:11:48 +0200

    create Blog component & adding style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GalleryComponent } from './component/gallery/gallery.component';
+import { BlogComponent } from './component/blog/blog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GalleryComponent
+    GalleryComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,
