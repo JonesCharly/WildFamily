bd33615c4a8088eea44b501f3aec9ab21d26afc1
Charline Fazentieux <chafazentieuxpro@gmail.com>
2019-05-11 21:54:35 +0200

    adding header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { GalleryComponent } from './component/gallery/gallery.component';
 import { BlogComponent } from './component/blog/blog.component';
+import { ContactUsComponent } from './component/contact-us/contact-us.component';
+import { AboutUsComponent } from './component/about-us/about-us.component';
 
 
 const routes: Routes = [
   {path: 'gallery', component: GalleryComponent },
-  {path: 'blog', component: BlogComponent }
+  {path: 'blog', component: BlogComponent },
+  {path: 'contact', component: ContactUsComponent },
+  {path: 'about', component: AboutUsComponent }
 ];
 
 @NgModule({
